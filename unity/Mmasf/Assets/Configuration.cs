8b3d9688d3405f4786fa5e0d69238c9650ac91f9
hahoyer <hahoyer@gmail.com>
2016-12-08 09:14:06 +0100

    New: Visualisation of configurations

---
+++ unity/Mmasf/Assets/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Configuration : MonoBehaviour
+{
+    public Text PortalName;
+    void Start() { PortalName.text = "This portal!!!"; }
+}
