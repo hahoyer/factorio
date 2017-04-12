ffcea3418009fbe4573a20919a172a95f61d2d00
hahoyer <hahoyer@gmail.com>
2017-04-12 09:51:07 +0200

    same

--- src/Mmasf/MmasfContext.cs
+++ src/Mmasf/MmasfContext.cs
@@ -25,7 +25,8 @@ namespace ManageModsAndSavefiles
             ConfigurationCache = new CompoundCache<Configuration>(Configuration.Create);
             ModConfigurationCache = new CompoundCache<ModConfiguration>(ModConfiguration.Create);
 
-            ModDictionary = new FunctionCache<string, FunctionCache<Version, ModDescription>>(CreateModDescription);
+            ModDictionary = new FunctionCache<string, FunctionCache<Version, ModDescription>>
+                (CreateModDescription);
 
             SystemConfigurationCache = new CompoundCache<SystemConfiguration>
             (
@@ -42,16 +43,15 @@ namespace ManageModsAndSavefiles
 
             UserConfigurationsCache = new CompoundCache<UserConfiguration[]>
             (
-                () => Configuration
-                    .UserConfigurationPaths
-                    .Select
-                    (
-                        item =>
-                            UserConfiguration.Create
-                                (item, Configuration.UserConfigurationPaths, this))
-                    .ToArray(),
-                UserConfigurationsCache
-            );
+                () =>
+                    Configuration
+                        .UserConfigurationPaths
+                        .Select
+                        (
+                            item =>
+                                UserConfiguration.Create
+                                    (item, Configuration.UserConfigurationPaths, this))
+                        .ToArray());
         }
 
         [DisableDump]
@@ -72,7 +72,8 @@ namespace ManageModsAndSavefiles
             {
                 var result = "";
 
-                var executable = Configuration.SystemPath.ToSmbFile().DirectoryName
+                var executable = Configuration.SystemPath.ToSmbFile()
+                    .DirectoryName
                     .PathCombine("bin\\x64\\factorio.exe");
 
                 result += "Executable path: " + executable + "\n";
@@ -86,12 +87,14 @@ namespace ManageModsAndSavefiles
         }
 
         static FunctionCache<Version, ModDescription> CreateModDescription(string name)
-            => new FunctionCache<Version, ModDescription>(version => new ModDescription(name, version));
+            => new FunctionCache<Version, ModDescription>
+                (version => new ModDescription(name, version));
 
         public void RenewUserConfigurationPaths()
         {
             Configuration.RenewUserConfigurationPaths();
             ConfigurationCache.Invalidate();
+            UserConfigurationsCache.Invalidate();
         }
 
         internal ModDescription CreateModReferenceBefore014(int i, BinaryRead reader)
@@ -109,12 +112,11 @@ namespace ManageModsAndSavefiles
                 return CreateModReferenceBefore014(i, reader);
 
             var name = reader.GetNextString<byte>();
-	        // ReSharper disable once UnusedVariable
+            // ReSharper disable once UnusedVariable
             var lookAhead = reader.GetBytes(150);
             var version = new Version
                 (reader.GetNext<byte>(), reader.GetNext<byte>(), reader.GetNext<byte>());
             return ModDictionary[name][version];
         }
-
     }
 }
