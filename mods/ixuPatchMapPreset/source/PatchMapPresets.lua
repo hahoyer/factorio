6be2bee897f8e1673dedcb037aefacb87f812a08
hahoyer.Anja <hahoyer@gmail.com>
2020-03-23 17:43:45 +0100

    feature: Settings introduced

--- mods/ixuPatchMapPreset/source/PatchMapPresets.lua
+++ mods/ixuPatchMapPreset/source/PatchMapPresets.lua
@@ -1,11 +1,14 @@
 require("util")
 
 function PatchMapPresets()
-    local mapGenPresets = data.raw["map-gen-presets"].default
-    for key,value in pairs(mapGenPresets) do
-        --log("before: "..key .. "=" .. serpent.block(value))
-        PatchMapPreset(mapGenPresets, key)
-        --log("after: "..key .. "=" .. serpent.block(value))
+    if settings.startup.ixuMatchMapPreset_ForceNoEnemies.value 
+    or settings.startup.ixuMatchMapPreset_ForceNoCliffs.value 
+    or settings.startup.ixuMatchMapPreset_ForceResearchQueue.value 
+    then
+        local mapGenPresets = data.raw["map-gen-presets"].default
+        for key,value in pairs(mapGenPresets) do
+            PatchMapPreset(mapGenPresets, key)
+        end
     end
 end
 
@@ -22,9 +25,15 @@ function PatchMapPreset(target, key)
     EnsureTableForKey(target[key], "advanced_settings")
     EnsureTableForKey(target[key].advanced_settings, "difficulty_settings")
 
+    if settings.startup.ixuMatchMapPreset_ForceNoEnemies.value then
     target[key].basic_settings.autoplace_controls["enemy-base"].size = "none"
-    target[key].basic_settings.cliff_settings = { richness = 0 }
-    target[key].advanced_settings.difficulty_settings.research_queue_setting = "always"
+    end
+    if settings.startup.ixuMatchMapPreset_ForceNoCliffs.value then
+        target[key].basic_settings.cliff_settings = { richness = 0 }
+    end
+    if settings.startup.ixuMatchMapPreset_ForceResearchQueue.value then
+        target[key].advanced_settings.difficulty_settings.research_queue_setting = "always"
+    end
 end
 
 function EnsureTableForKey(target, key)
---
+++ mods/ixuPatchMapPreset/source/settings.lua
@@ -0,0 +1,20 @@
+data:extend{
+  {
+    type = "bool-setting",
+    name = "ixuMatchMapPreset_ForceNoEnemies",
+    default_value = true,
+    setting_type = "startup",
+  },
+  {
+    type = "bool-setting",
+    name = "ixuMatchMapPreset_ForceNoCliffs",
+    default_value = true,
+    setting_type = "startup",
+  },
+  {
+    type = "bool-setting",
+    name = "ixuMatchMapPreset_ForceResearchQueue",
+    default_value = true,
+    setting_type = "startup",
+  },
+}
