429834b19e5c748cf212814e70f9b90019742ae2
hahoyer.Anja <hahoyer@gmail.com>
2020-05-24 21:03:10 +0200

    change: some cleanup

--- mods/HHTraining/constants.lua
+++
@@ -1,6 +0,0 @@
-local result = {}
-
-result.ModName = "HHTraining"
-result.GraphicsPath = "__"..result.ModName.."__/graphics/"
-
-return result
--- mods/HHTraining/data.lua
+++
@@ -1,6 +0,0 @@
-data.raw["character"]["character"].build_distance = 10000
-data.raw["character"]["character"].reach_distance = 10000
-data.raw["character"]["character"].reach_resource_distance = 10000
-data.raw["character"]["character"].drop_item_distance = 10000
-
-data.raw["character"]["character"].inventory_size = 240
