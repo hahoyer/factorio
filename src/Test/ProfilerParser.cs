066c009c2215eada5f25d80a52c571e9f7cc9edb
hahoyer.Anja <hahoyer@gmail.com>
2020-08-19 11:01:47 +0200

    test: new test

---
+++ src/Test/ProfilerParser.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using hw.Helper;
+using ManageModsAndSaveFiles;
+
+namespace Test
+{
+    sealed class ProfilerParser
+    {
+        public ProfilerParser(string configurationName)
+        {
+            var configuration = MmasfContext
+                .Instance
+                .UserConfigurations
+                .Single(context=> context.Name == configurationName);
+
+            var y = new LogfileWatcher(configuration.Path);
+
+            4.Seconds().Sleep();
+
+            var yy = y.Lines.Where(line => line.LineData is ScriptLine).ToArray();
+        }
+
+        public void Format() {}
+    }
+}
