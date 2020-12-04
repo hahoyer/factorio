5ac0c12816af03e67a8d380d24b9a96cf334f0c5
hahoyer.Anja <hahoyer@gmail.com>
2020-12-04 14:24:22 +0100

    Version: 0.2.2 Date: 2020-12-04   Changes:     - using mod-gui     - clock line moved from bottom to left ---------------------------------------------------------------------------------------------------

--- mods/ixuClock/source/FormatPlaytimeInformation.lua
+++ mods/ixuClock/source/FormatPlaytimeInformation.lua
@@ -1,9 +1,10 @@
+local mod_gui = require("mod-gui")
 require("util")
 
 local play_time_seconds = -1
 
 function FormatPlaytimeInformation(player)
-    local ticks  = game.tick
+    local ticks = game.tick
     local daytime = player.surface.daytime + 0.5
     local crashdays = game.tick / player.surface.ticks_per_day
 
@@ -12,26 +13,19 @@ function FormatPlaytimeInformation(player)
     local hours = math.floor(minutes / 60)
     local days = math.floor(hours / 24)
     local hourString = ""
-    if hours > 0 then
-        hourString = string.format("%d:", hours % 24)
-    end
+    if hours > 0 then hourString = string.format("%d:", hours % 24) end
     local dayString = ""
-    if days > 0 then
-        dayString = string.format("%d.", days)
-    end
+    if days > 0 then dayString = string.format("%d.", days) end
     local playtimeString = string.format("%02d:%02d", minutes % 60, seconds % 60)
 
-    local crashdaysString  = string.format("%d", crashdays);
+    local crashdaysString = string.format("%d", crashdays);
 
     local dayminutes = math.floor(daytime * 24 * 60) % 60
-    local dayhour = math.floor(daytime * 24 ) % 24
+    local dayhour = math.floor(daytime * 24) % 24
     local daytimeString = string.format("%02d:%02d", dayhour, dayminutes)
 
-    return dayString
-            .. hourString
-            .. playtimeString .. " "
-            .. crashdaysString .. " "
-            .. daytimeString
+    return dayString .. hourString .. playtimeString .. " " .. crashdaysString .. " "
+               .. daytimeString
 end
 
 function ShowFormatPlaytimeInformation(event)
@@ -39,19 +33,18 @@ function ShowFormatPlaytimeInformation(event)
 
     play_time_seconds = math.floor(game.tick / 60)
 
-    if previous == play_time_seconds then
-        return
-    end
-
+    if previous == play_time_seconds then return end
 
     for i, player in pairs(game.connected_players) do
-        if player.gui.top.hwclock == nil then
-            player.gui.top.add { type = "label", name = "hwclock" }
-        end
+        -- remove legacy clock line
+        if player.gui.top.hwclock then player.gui.top.hwclock.destroy() end
 
+        local hwclock = mod_gui.get_frame_flow(player).hwclock
+        if not hwclock then
+            hwclock = mod_gui.get_frame_flow(player).add {type = "label", name = "hwclock"}
+        end
         local value = FormatPlaytimeInformation(player)
-        player.gui.top.hwclock.caption = value
+        hwclock.caption = value
     end
 end
-
 
