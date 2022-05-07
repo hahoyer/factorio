ba3a5cfd439ad17bd7f4b775b8309339d6393ed1
hh <hahoyer@gmail.com>
2022-05-07 12:33:54 +0200

    change: Classes

---
+++ src/FApi/AssessmentArea.cs
@@ -0,0 +1,56 @@
+﻿using hw.DebugFormatter;
+using hw.Helper;
+using HWBase;
+
+namespace FactorioApi;
+
+sealed class AssessmentArea<T> : DumpableObject
+{
+    internal interface IConfiguration
+    {
+        T Default { get; }
+        string JsonPath { get; }
+        T GetNewValue(T old);
+        void Log(T result);
+    }
+
+    readonly IConfiguration Configuration;
+    readonly ValueCache<T> NewCache;
+    readonly ValueCache<T> OldCache;
+    readonly ValueCache<T> CurrentCache;
+    readonly ValueCache<SmbFile> File;
+
+    public AssessmentArea(IConfiguration configuration)
+    {
+        Configuration = configuration;
+        OldCache = new(GetOldValue);
+        NewCache = new(() => Configuration.GetNewValue(OldCache.Value));
+        CurrentCache = new(GetCurrentValue);
+        File = new(GetFile);
+    }
+
+    internal T Current => CurrentCache.Value;
+
+    T GetCurrentValue()
+    {
+        if(NewCache.Value == null)
+            return OldCache.Value;
+
+        var result = NewCache.Value;
+        File.Value.String = result.ToJSon();
+        "------------------------".Log();
+        $"New assessment saved to {File.Value.FullName}".Log();
+        Configuration.Log(result);
+        return result;
+    }
+
+    T GetOldValue() => File.Value.String.FromJson<T>();
+
+    SmbFile GetFile()
+    {
+        var result = Configuration.JsonPath.ToSmbFile() / "Assessment" / (typeof(T).Name + ".json");
+        if(!result.Exists)
+            result.String = Configuration.Default.ToJSon();
+        return result;
+    }
+}
--- src/FApi/Assessments.cs
+++ src/FApi/Assessments.cs
@@ -2,70 +2,19 @@
 using FactorioApi.Assessment;
 using hw.DebugFormatter;
 using hw.Helper;
-using HWBase;
 
 namespace FactorioApi;
 
 sealed class Assessments
     : DumpableObject
-        , Assessments.Area<Classes>.IConfiguration
-        , Assessments.Area<Members>.IConfiguration
+        , AssessmentArea<Classes>.IConfiguration
+        , AssessmentArea<Members>.IConfiguration
 {
-    sealed class Area<T> : DumpableObject
-    {
-        internal interface IConfiguration
-        {
-            T Default { get; }
-            string JsonPath { get; }
-            T GetNewValue();
-            void Log();
-        }
-
-        readonly IConfiguration Configuration;
-        readonly ValueCache<T> NewCache;
-        readonly ValueCache<T> PersistentCache;
-        readonly ValueCache<SmbFile> File;
-
-        public Area(IConfiguration configuration)
-        {
-            Configuration = configuration;
-            PersistentCache = new(GetPersistentValue);
-            NewCache = new(Configuration.GetNewValue);
-            File = new(GetFile);
-        }
-
-        internal T Persistent => PersistentCache.Value;
-
-        internal T New => NewCache.Value;
-
-        internal void Save()
-        {
-            if(New == null)
-                return;
-
-            var result = New;
-            File.Value.String = result.ToJSon();
-            "------------------------".Log();
-            $"New assessment saved to {File.Value.FullName}".Log();
-            Configuration.Log();
-        }
-
-        T GetPersistentValue() => File.Value.String.FromJson<T>();
-
-        SmbFile GetFile()
-        {
-            var result = Configuration.JsonPath.ToSmbFile() / "Assessment" / (typeof(T).Name + ".json");
-            if(!result.Exists)
-                result.String = Configuration.Default.ToJSon();
-            return result;
-        }
-    }
-
     readonly string JsonPath;
     readonly GameApi GameApi;
 
-    readonly Area<Classes> Classes;
-    readonly Area<Members> Members;
+    readonly AssessmentArea<Classes> Classes;
+    readonly AssessmentArea<Members> Members;
 
     public Assessments(string jsonPath, GameApi gameApi)
     {
@@ -75,11 +24,10 @@ sealed class Assessments
         Members = new(this);
     }
 
-    Classes Area<Classes>.IConfiguration.Default => new() { FactorioVersion = GameApi.Version };
+    Classes AssessmentArea<Classes>.IConfiguration.Default => new() { FactorioVersion = GameApi.Version };
 
-    Classes Area<Classes>.IConfiguration.GetNewValue()
+    Classes AssessmentArea<Classes>.IConfiguration.GetNewValue(Classes classes)
     {
-        var classes = Classes.Persistent;
         var relevantClasses = classes.Relevant ?? new string[0];
         var irrelevantClasses = classes.Irrelevant ?? new string[0];
         var knownClasses = relevantClasses.Concat(irrelevantClasses).ToArray();
@@ -103,86 +51,55 @@ sealed class Assessments
         return result;
     }
 
-    string Area<Classes>.IConfiguration.JsonPath => JsonPath;
+    string AssessmentArea<Classes>.IConfiguration.JsonPath => JsonPath;
 
-    void Area<Classes>.IConfiguration.Log()
+    void AssessmentArea<Classes>.IConfiguration.Log(Classes result)
     {
         "------------------------".Log();
         "New classes: ".Log();
-        var newClasses = Classes.New.New;
+        var newClasses = result.New;
         newClasses.Stringify("\n").Log();
         "------------------------".Log();
-        $"  Classes: {Classes.New.Irrelevant.Length + Classes.New.Relevant.Length}".Log();
-        $"    relevant: {Classes.New.Relevant.Length}".Log();
-        $"    irrelevant: {Classes.New.Irrelevant.Length}".Log();
+        $"  Classes: {result.Irrelevant.Length + result.Relevant.Length}".Log();
+        $"    relevant: {result.Relevant.Length}".Log();
+        $"    irrelevant: {result.Irrelevant.Length}".Log();
         $"    new: {newClasses.Length}".Log();
         "------------------------".Log();
     }
 
-    Members Area<Members>.IConfiguration.Default
+    Members AssessmentArea<Members>.IConfiguration.Default
         => new()
         {
             FactorioVersion = GameApi.Version
         };
 
-    Members Area<Members>.IConfiguration.GetNewValue()
+    Members AssessmentArea<Members>.IConfiguration.GetNewValue(Members members)
     {
-        var classes = Classes.Persistent;
-        var relevantClasses = classes.Relevant ?? new string[0];
-        var irrelevantClasses = classes.Irrelevant ?? new string[0];
-        var knownClasses = relevantClasses.Concat(irrelevantClasses).ToArray();
-
-        var members = Members.Persistent;
-        var relevantMembers = members.Relevant ?? new string[0];
-        var irrelevantMembers = members.Irrelevant ?? new string[0];
-        var knownMembers = relevantMembers.Concat(irrelevantMembers).ToArray();
-
-        var special = Members.Persistent.Specific.ToDictionary(m => m.ClassName);
-
-        var classGrouping = GameApi
-            .Classes
-            .GroupBy(c => c.Name.In(knownClasses)? (bool?)null : special[c.Name] == null);
-
-        var newClasses = GameApi
-            .Classes
-            .Where(c => !c.Name.In(knownClasses))
-            .Select(c => c.Name)
-            .ToArray();
-
-        if(!newClasses.Any())
-            return null;
-
-        var result = new Members
-        {
-            FactorioVersion = GameApi.Version
-            , Irrelevant = irrelevantMembers
-            , Relevant = relevantMembers
-            , New = newClasses
-        };
-        return result;
+        NotImplementedMethod(members);
+        return default;
     }
 
-    string Area<Members>.IConfiguration.JsonPath => JsonPath;
+    string AssessmentArea<Members>.IConfiguration.JsonPath => JsonPath;
 
-    void Area<Members>.IConfiguration.Log()
+    void AssessmentArea<Members>.IConfiguration.Log(Members result)
     {
         "------------------------".Log();
         "New members: ".Log();
-        var newClasses = Classes.New.New;
+        var newClasses = result.New;
         newClasses.Stringify("\n").Log();
         "------------------------".Log();
-        $"  Members: {Classes.New.Irrelevant.Length + Classes.New.Relevant.Length}".Log();
-        $"    relevant: {Classes.New.Relevant.Length}".Log();
-        $"    irrelevant: {Classes.New.Irrelevant.Length}".Log();
+        $"  Members: {result.Irrelevant.Length + result.Relevant.Length}".Log();
+        $"    relevant: {result.Relevant.Length}".Log();
+        $"    irrelevant: {result.Irrelevant.Length}".Log();
         $"    new: {newClasses.Length}".Log();
         "------------------------".Log();
     }
 
-    public bool IsRelevant(Class arg) => arg.Name.In(Classes.New.Relevant);
+    public bool IsRelevant(Class arg) => arg.Name.In(Classes.Current.Relevant);
 
     public bool IsRelevant(Class argClass, Field argField)
     {
-        var specific = Members.New.Specific.SingleOrDefault(s => s.ClassName == argClass.Name);
+        var specific = Members.Current.Specific.SingleOrDefault(s => s.ClassName == argClass.Name);
         if(specific != null)
         {
             if(argField.Name.In(specific.Relevant))
@@ -191,6 +108,6 @@ sealed class Assessments
                 return false;
         }
 
-        return argField.Name.In(Members.New.Relevant);
+        return argField.Name.In(Members.Current.Relevant);
     }
 }
