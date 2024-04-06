8f1406f7e66c231a15a3bce0f7ed5647a49a3243
Harald Herbert Hoyer <hahoyer@gmail.com>
2024-04-06 17:07:20 +0200

    refactor: upgrade C#

--- src/Mmasf/StreamWithCleanupList.cs
+++ src/Mmasf/StreamWithCleanupList.cs
@@ -3,41 +3,32 @@ using System.IO;
 
 namespace ManageModsAndSaveFiles;
 
-public sealed class StreamWithCleanupList : Stream
+public sealed class StreamWithCleanupList(Stream streamImplementation, params IDisposable[] others) : Stream
 {
-    readonly IDisposable[] Others;
-    readonly Stream StreamImplementation;
+    public override void Flush() => streamImplementation.Flush();
+    public override long Seek(long offset, SeekOrigin origin) => streamImplementation.Seek(offset, origin);
+    public override void SetLength(long value) => streamImplementation.SetLength(value);
 
-    public StreamWithCleanupList(Stream streamImplementation, params IDisposable[] others)
-    {
-        StreamImplementation = streamImplementation;
-        Others = others;
-    }
-
-    public override void Flush() => StreamImplementation.Flush();
-    public override long Seek(long offset, SeekOrigin origin) => StreamImplementation.Seek(offset, origin);
-    public override void SetLength(long value) => StreamImplementation.SetLength(value);
-
-    public override int Read(byte[] buffer, int offset, int count) => StreamImplementation.Read(buffer, offset, count);
+    public override int Read(byte[] buffer, int offset, int count) => streamImplementation.Read(buffer, offset, count);
 
     public override void Write
-        (byte[] buffer, int offset, int count) => StreamImplementation.Write(buffer, offset, count);
+        (byte[] buffer, int offset, int count) => streamImplementation.Write(buffer, offset, count);
 
-    public override bool CanRead => StreamImplementation.CanRead;
-    public override bool CanSeek => StreamImplementation.CanSeek;
-    public override bool CanWrite => StreamImplementation.CanWrite;
-    public override long Length => StreamImplementation.Length;
+    public override bool CanRead => streamImplementation.CanRead;
+    public override bool CanSeek => streamImplementation.CanSeek;
+    public override bool CanWrite => streamImplementation.CanWrite;
+    public override long Length => streamImplementation.Length;
 
     public override long Position
     {
-        get => StreamImplementation.Position;
-        set => StreamImplementation.Position = value;
+        get => streamImplementation.Position;
+        set => streamImplementation.Position = value;
     }
 
     protected override void Dispose(bool disposing)
     {
         if(disposing)
-            foreach(var other in Others)
+            foreach(var other in others)
                 other.Dispose();
 
         base.Dispose(disposing);
