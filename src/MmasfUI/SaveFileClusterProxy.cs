22575db505f280c7e303bc6860f4e837f48f9ef0
Harald Herbert Hoyer <hahoyer@gmail.com>
2024-04-07 22:13:01 +0200

    refactor: reformat

--- src/MmasfUI/SaveFileClusterProxy.cs
+++ src/MmasfUI/SaveFileClusterProxy.cs
@@ -17,56 +17,61 @@ sealed class SaveFileClusterProxy : INotifyPropertyChanged
 
     readonly FileCluster Data;
     readonly string ConfigurationName;
-    FileCluster DataIfRead => Data.IsValidData ? Data : null;
+    FileCluster DataIfRead => Data.IsValidData? Data : null;
     IEnumerable<ModConflict> Conflicts => DataIfRead?.RelevantConflicts;
 
     [UsedImplicitly]
     public DateTime Created => Data.Created;
+
     [UsedImplicitly]
     public string Name => Data.Name;
 
     [UsedImplicitly]
-    public TimeSpanProxy Duration { get; set; }
+    public TimeSpanProxy Duration;
+
     [UsedImplicitly]
     public Version Version => DataIfRead?.Version;
+
     [UsedImplicitly]
     public string GamePart => DataIfRead?.ScenarioName + "/" + DataIfRead?.MapName + "/" + DataIfRead?.CampaignName;
+
     [UsedImplicitly]
     public bool IsKnownConflict => Conflicts?.Any(c => c.IsKnown) ?? false;
+
     [UsedImplicitly]
     public string FirstUnknown
     {
         get
         {
-                var conflicts = Conflicts?.ToArray();
-                if(conflicts == null)
-                    return null;
-                if(conflicts.Any(c => c.IsKnown))
-                    return null;
-                return conflicts.FirstOrDefault(c => !c.IsKnown)?.Mod.FullName;
-            }
+            var conflicts = Conflicts?.ToArray();
+            if(conflicts == null)
+                return null;
+            if(conflicts.Any(c => c.IsKnown))
+                return null;
+            return conflicts.FirstOrDefault(c => !c.IsKnown)?.Mod.FullName;
+        }
     }
-
-    public SaveFileClusterProxy(FileCluster data, string configurationName)
-    {
-            Data = data;
-            ConfigurationName = configurationName;
-        }
-
-    public void Refresh()
-    {
-            Data.IsDataRead = true;
-            Duration = new TimeSpanProxy(Data.Duration);
-            OnPropertyChanged();
-        }
-
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    void OnPropertyChanged()
-        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
 
     [Command(Command.ViewConflicts)]
     public bool IsConflict => Conflicts?.Any() ?? false;
+
+    public SaveFileClusterProxy(FileCluster data, string configurationName)
+    {
+        Data = data;
+        ConfigurationName = configurationName;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void Refresh()
+    {
+        Data.Refresh();
+        Duration = new(Data.Duration);
+        OnPropertyChanged();
+    }
+
+    void OnPropertyChanged()
+        => PropertyChanged?.Invoke(this, new(null));
 
     [Command(Command.ViewConflicts)]
     public void ViewConflicts()
